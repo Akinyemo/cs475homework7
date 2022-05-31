d0e06f1e97fa970741fa360dbc27a96ebabe0571
Bolaji Akinyemi <53135944+Akinyemo@users.noreply.github.com>
2022-05-30 23:17:44 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ for t in 1024 4096 16384 65536 262144 1048576 2097152 419430
 do
         for b in 8 32 128
         do
-                g++ -o first first.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
-                ./first -DNMB=$t -DLOCAL_SIZE=$b 
+                g++  -DNMB=$t -DLOCAL_SIZE=$b  -o first first.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1  -lm -fopenmp
+                ./first 
                 
         done
 done
