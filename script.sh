e734f2c05a8ca2ab86ee167772c53e6bfddd7106
Bolaji Akinyemi <53135944+Akinyemo@users.noreply.github.com>
2022-05-30 22:54:23 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ for t in 1024 4096 16384 65536 262144 1048576 2097152 4194304
 do
         for b in 8 32 128
         do
-                /usr/local/apps/cuda/cuda-10.1/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o montecarlo  proj05.cu
-                ./montecarlo
+                g++ -o first first.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+                ./first -DNUMTRIALS=$t -DBLOCKSIZE=$b 
+                
         done
 done
